00c837230a963ac60436e38fcd0b6e17b0d493f3
krishna <keshri819@gmail.com>
2022-03-11 00:10:31 +0530

    Modified Data

--- src/data/english.js
+++ src/data/english.js
@@ -16,16 +16,16 @@ const dataJSON = {
     title: "About",
     paragraph1: (
       <>
-        My name is Krishna K. Keshri, I'm a <p>Hotel Management Graduate </p> at
-        Ramaiah University, Bangalore. I'm a{" "}
+        My name is Krishna K. Keshri, I'm a <p>Hotel Management Graduate </p>{" "}
+        from Ramaiah University, Bangalore. I'm a{" "}
         <p>MERN Stack Development enthusiast</p> and I enjoy creating things
-        that live on the internet. My interest in web development started back
-        last year when I was trying to edit things on the web — taught me a lot
-        about HTML & CSS!.
+        that live on the internet. My interest in web development started last
+        year when I was trying to edit things on the web — taught me a lot about
+        HTML & CSS!.
       </>
     ),
     paragraph2:
-      "My goal is to cooperate with the creation of tools that contribute to the development of a person, a company, or a nation. That's why I like to learn new technologies, and use them with the mission to improve the world a little bit, one step at a time. \n Fast Forwarding to today, I built a number of web applications and 4 major projects. Learned a great deal about teamwork, leadership, and communication. After months of rigorous training, here I am looking for an opportunity as a full stack web developer.",
+      "My goal is to cooperate with the creation of tools that contribute to the development of a person, a company, or a nation. That's why I like to learn new technologies, and use them with the mission to improve the world a little bit, one step at a time. \n Fast Forwarding till today, I built numbers of web applications and 4 major projects. Learned a lot about teamwork, leadership, and communication. After months of rigorous training, here I am looking for an opportunity as a full stack web developer.",
     cvTitle: "Download CV",
     cvFile: "",
   },
